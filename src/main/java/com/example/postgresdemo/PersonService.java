6b8a53d057f630e20edfd4bff8af111bd40f6eed
kangyao.ma@sap.com <kangyao.ma@sap.com>
2021-01-24 16:08:41 +0800

    add list & update

--- src/main/java/com/example/postgresdemo/PersonService.java
+++ src/main/java/com/example/postgresdemo/PersonService.java
@@ -4,10 +4,20 @@ package com.example.postgresdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
     public Person create(Person person){return personRepository.save(person);}
+
+    public List<Person> list(){
+        return personRepository.findAll();
+    }
+
+    public Person update(Person person){
+        return personRepository.saveAndFlush(person);
+    }
 }
--- src/main/java/com/example/postgresdemo/PostgresdemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresdemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @SpringBootApplication
 public class PostgresdemoApplication {
@@ -19,14 +21,22 @@ public class PostgresdemoApplication {
 		SpringApplication.run(PostgresdemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "world") String name){
+	@GetMapping()
+	public String index(@RequestParam(value = "name", defaultValue = "world") String name){
 		return String.format("Hello, %s", name);
 	}
 
 	@GetMapping("/create")
 	public Person create(@ModelAttribute Person person){
 		return personService.create(person);
+	}
+
+	@GetMapping("/list")
+	public List<Person> list(){return personService.list();}
+
+	@GetMapping("/update")
+	public Person update(@ModelAttribute Person person){
+		return personService.update(person);
 	}
 
 	@GetMapping("/fakecreate")
